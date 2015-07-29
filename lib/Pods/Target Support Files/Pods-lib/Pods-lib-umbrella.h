df8501b3f48cea6daeba2c9382dc93a111ff5817
Vincent Min Tseng <mintseng@berkeley.edu>
2015-07-29 16:51:57 -0700

    pods set up, integrating subscription

---
+++ lib/Pods/Target Support Files/Pods-lib/Pods-lib-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_libVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_libVersionString[];
+
--- lib/subscription/Subscription.swift
+++ lib/subscription/Subscription.swift
@@ -2,4 +2,86 @@ import Foundation
 
 class Subscription {
     
+    private let platform: Platform
+    private var pubnub: PubNub?
+    private var eventFilters: [String]
+    private var subscription: ISubscription
+    
+    
+    init(platform: Platform) {
+        
+    }
+    
+    struct IDeliveryMode {
+        let transportType: String
+        let encryption: Bool
+        let address: String
+        let subscriperKey: String
+        let secretKey: String
+    }
+    
+    struct ISubscription {
+        let eventFilters: [String]
+        let expirationTime: String
+        let expiresIn: NSNumber
+        let deliveryMode: IDeliveryMode
+        let id: String
+        let creationTime: String
+        let status: String
+        let uri: String
+    }
+    
+    func addEvents(events: [String]) -> Subscription {
+        
+    }
+    
+    func setevents(events: [String]) -> Subscription {
+        
+    }
+    
+    func register(options: [String: AnyObject]) {
+        
+    }
+    
+    func renew(options: [String: AnyObject]) {
+        
+    }
+    
+    func isSubscribed() -> Bool {
+        
+    }
+    
+    func on(event: String, callback: (args: AnyObject...) -> ()) -> Subscription {
+        
+    }
+    
+    func off(event: String, callback: (args: AnyObject...) -> ()) -> Subscription {
+        
+    }
+    
+    func emit(event: String) -> AnyObject {
+        
+    }
+    
+    private func getFullEventFilters() -> [String] {
+        return eventFilters
+    }
+    
+    private func updateSubscription(subscription: ISubscription) {
+    
+    }
+    
+    private func unsubscribe() {
+        
+    }
+    
+    private func subscribeAtPubnub() {
+        
+    }
+    
+    private func notify() {
+        
+    }
+    
+    
 }
